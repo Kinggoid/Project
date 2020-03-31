a75f06686344b80eac9f1a08c0c940016fa20e52
Thijme Wortel <60653407+Kinggoid@users.noreply.github.com>
2020-03-31 13:39:12 +0200

    update test 2

--- 1.py
+++ 1.py
@@ -1,2 +1,17 @@
-print('0h')
-print('print')
+import psycopg2
+
+c = psycopg2.connect("dbname=kennis user=postgres password=Tomaat221")  #: edit this.
+cur = c.cursor()
+
+filenames = ['samen_gekocht']
+
+for filename in filenames:
+    print(filename)
+    with open(filename + '.csv') as csvfile:
+        print("Copying {}...".format(filename))
+        cur.copy_expert("COPY "+filename+" FROM STDIN DELIMITER ',' CSV HEADER", csvfile)
+        c.commit()
+
+c.commit()
+cur.close()
+c.close()
