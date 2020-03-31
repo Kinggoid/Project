5621061d368ad625a6ed1c650ca1da64ff04bb4f
Thijme Wortel <60653407+Kinggoid@users.noreply.github.com>
2020-03-31 16:58:15 +0200

    Aanbieding.py

--- Algoritmes/Aanbieding.py
+++ Algoritmes/Aanbieding.py
@@ -1,52 +1,51 @@
 import csv
 import psycopg2
 
-c = psycopg2.connect("dbname=HUwebshop user=postgres password=Tomaat221")
+c = psycopg2.connect("dbname=(voer naam van je database in) user=postgres password=(voer je wachtwoord in)") # Vul zelf in
 curs = c.cursor()
 
 
-def populair(cursor):
+def aanbieding(cursor):
+    """In deze definitie kijken we welke populaire producten ook in de aanbieding zijn en zetten die in een .csv bestand"""
     with open('aanbieding.csv', 'w', newline='') as csvout:
         fieldnames = ['productid', 'productnaam', 'aanbieding']
         writer = csv.DictWriter(csvout, fieldnames=fieldnames)
         writer.writeheader()
 
         cursor.execute("""select producten_id from product_gekocht""")
-        prod = cursor.fetchall()
+        prod = cursor.fetchall() # Kijkt hoe vaak elk product is gekocht
 
         cursor.execute("""select id from producten""")
-        producten = cursor.fetchall()
+        producten = cursor.fetchall() # Pakt één van elk product
+        
         populair = []
-
-        for i in producten:
-            populair.append([prod.count(i), i[0]])
-
         optellen = []
 
-        for i in populair:
-            optellen.append(i[0])
-
+        for i in producten: # Kijkt hoe vaak een product gekocht is en over welk product dit gaat en plaatst dit in verschillende lists
+            optellen.append(prod.count(i))
+            populair.append(i[0])
+            
         top10 = []
 
         while len(top10) < 11:
-            x = optellen.index(max(optellen))
-            prod = populair[x]
+            maxi = optellen.index(max(optellen)) # Op welke index staat het product dat het vaakst voorkomt
+            prod = populair[maxi] # Welk product staat op die index (dus is het populairst)
 
-            cursor.execute("""select aanbieding from producten where id = (%s)""", (prod[1], ))
-            produ = cursor.fetchall()
+            cursor.execute("""select aanbieding from producten where id = (%s)""", (prod, ))
+            produ = cursor.fetchall() # Kijkt of dit product een aanbieding heeft
 
             try:
-                if produ[0][0] == 'None' or produ[0][0] == None:
-                    optellen.remove(optellen[x])
+                if produ[0][0] == 'None' or produ[0][0] == None: # Zo nee, dan wissen we hem en gaan we door
+                    optellen.remove(optellen[maxi])
                     populair.remove(prod)
                 else:
-                    top10.append([prod[1], produ[0][0]])
-                    optellen.remove(optellen[x])
+                    top10.append([prod[1], produ[0][0]]) # Zo ja, dan slaan we hem op, wissen we hem en gaan we door
+                    optellen.remove(optellen[maxi])
                     populair.remove(prod)
             except:
                 continue
 
-        for top in top10:
+        for top in top10: # Schrijft de top tien populairste producten in een .csv bestand
             cursor.execute("""select naam from producten where id = (%s)""", (top[0],))
             naam = cursor.fetchall()
 
